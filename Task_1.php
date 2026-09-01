78d3bad220c9716c390e0ac8d8eb4a8ee0a36267
Enamullah Moatasim <anamullah.afghan22@gmail.com>
2026-09-01 11:20:01 +0430

    Update student ID in Task_1.php

--- Task_1.php
+++ Task_1.php
@@ -1,13 +1,11 @@
 <?php
 
 // Full Name: Enamullah Moatasim
-// Student ID: YOUR_STUDENT_ID
-
-// Task 1
+// Student ID: 7
 
 class Library
 {
-    // This value is constant because the maximum number of books is fixed.
+    
     const MAX_BOOKS = 3;
 }
 
